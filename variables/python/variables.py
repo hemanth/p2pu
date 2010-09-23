227e847ea09114a97a066938337723d737aed045
Jeremy Hull <sourdrums@gmail.com>
2010-09-23 03:34:18 -0400

    Added python script for variables

---
+++ variables/python/variables.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Introduction to variables in python
+
+# Number variables
+num   = 5
+num2  = 10
+numfl = 1.5       # Float number
+
+print num         # print out what is contained in variable num
+print num + num2  # can do calculations within a print
+print
+
+# String variables
+string  = 'Single quote string'
+string2 = "Double quote string"
+
+print string                 # Both single and double quotes
+print string2                # are the same type
+
+print string +'. '+ string2  # Can concatenate strings aswell within print
+print
+
+# Types
+print type(num)
+print type(numfl)
+print type(string)
+print type(string2)
+print
+
+print 'I love p2pu '*5   # Repeats the print
